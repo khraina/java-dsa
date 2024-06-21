9adc2a48790f09c6c49aa133535ced1dc031d542
raina kh <84083826+khraina@users.noreply.github.com>
2024-06-21 13:02:47 +0000

    keys

--- hashtables/hash Get3/Main.java
+++ hashtables/hash Get3/Main.java
@@ -3,11 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         HashTable myHashTable = new HashTable();
-
+        //set key=value-pair in dictionary
         myHashTable.set("nails", 100);
         myHashTable.set("tile", 50);
         myHashTable.set("lumber", 80);
 
+        //get the value of the key
         System.out.println("Lumber:");
         System.out.println( myHashTable.get("lumber") );
 
