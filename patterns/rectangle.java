05b7e57d8f71701b957ad159ebe4f1d72f12bb70
raina kh <84083826+khraina@users.noreply.github.com>
2024-06-25 14:43:32 +0000

    patterns

---
+++ patterns/rectangle.java
@@ -0,0 +1,24 @@
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++)   //rows
+		{
+		    for(int j=1;j<=5;j++)  //coloumns
+		    {
+		        System.out.print("*");
+		    }
+		    
+		    System.out.println();
+		}
+	}
+}
+
+
+/*
+
+*****
+*****
+*****
+*****
+
+*/
